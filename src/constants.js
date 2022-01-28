e45c7f134886675e79d2e51335f5255032b45f9e
King Dev <kd19850511@gmail.com>
2022-01-28 07:19:01 -0500

    version update

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ import { getUser } from './services'
 
 const user = getUser()
 
-export const VERSION = 'v1.02'
+export const VERSION = 'v1.03'
 
 const USER_TYPES = {
   SUPER_ADMIN: 'super_admin',
