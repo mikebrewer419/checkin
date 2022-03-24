e7174229227996384eb33d9e3350c969be193262
King Dev <kd19850511@gmail.com>
2022-03-24 15:18:35 -0400

    ios detect update

--- src/views/Onboard/index.js
+++ src/views/Onboard/index.js
@@ -28,7 +28,7 @@ const mobileSafariCheck = () => {
   const ua = window.navigator.userAgent
   const iOS = !!ua.match(/iPad/i) || !!ua.match(/iPhone/i)
   const webkit = !!ua.match(/WebKit/i)
-  const iOSSafari = iOS && webkit && !ua.match(/CriOS/i)
+  const iOSSafari = iOS && webkit
   return iOSSafari
 }
 
